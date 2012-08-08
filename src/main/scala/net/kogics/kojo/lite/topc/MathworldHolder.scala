b23273fea68751084cf45ec2d451aa3cfcc710e1
Lalit Pant <pant.lalit@gmail.com>
2012-08-08 20:52:55 +0530

    Don't let D3 slow down resizing of Turtle/Mw Canvas.

--- src/main/scala/net/kogics/kojo/lite/topc/MathworldHolder.scala
+++ src/main/scala/net/kogics/kojo/lite/topc/MathworldHolder.scala
@@ -3,8 +3,20 @@ package net.kogics.kojo.lite.topc
 import javax.swing.JComponent
 import bibliothek.gui.dock.common.DefaultSingleCDockable
 import java.awt.Color
+import bibliothek.gui.dock.common.event.CFocusListener
+import bibliothek.gui.dock.common.intern.CDockable
+import net.kogics.kojo.core.KojoCtx
 
-class MathworldHolder(val mw: JComponent) extends DefaultSingleCDockable("MW", "Mathworld", mw) {
-	mw.setBackground(Color.white)
+class MathworldHolder(val mw: JComponent, ctx: KojoCtx) extends DefaultSingleCDockable("MW", "Mathworld", mw) {
+  mw.setBackground(Color.white)
+
+  this.addFocusListener(new CFocusListener {
+    override def focusGained(dockable: CDockable) {
+      ctx.mwActivated()
+    }
+
+    override def focusLost(dockable: CDockable) {
+    }
+  })
 }
 
