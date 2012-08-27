dd2dc3ed9f431085559c690718865c73ed809087
Lalit Pant <pant.lalit@gmail.com>
2012-08-27 11:46:15 +0530

    Enable error links.

--- src/main/scala/net/kogics/kojo/lite/Main.scala
+++ src/main/scala/net/kogics/kojo/lite/Main.scala
@@ -135,7 +135,7 @@ object Main extends AppMenu {
       codeSupport = CodeExecutionSupport.initedInstance(codePane, kojoCtx)
       val drawingCanvasH = new DrawingCanvasHolder(SpriteCanvas.instance, kojoCtx)
       scriptEditorH = new ScriptEditorHolder(new JPanel(), codePane, codeSupport)
-      val outputHolder = new OutputWindowHolder(codeSupport.outputWindow)
+      val outputHolder = new OutputWindowHolder(codeSupport.outputWindow, codeSupport.errorWindow, codeSupport.outPanel)
       val storyHolder = new StoryTellerHolder(StoryTeller.instance)
       val mwHolder = new MathworldHolder(GeoGebraCanvas.instance, kojoCtx)
       val d3Holder = new D3CanvasHolder(Canvas3D.instance, kojoCtx)
