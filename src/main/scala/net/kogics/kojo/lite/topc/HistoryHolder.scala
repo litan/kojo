b29ceb40d36573c63c520bad54553d7b5983697b
Lalit Pant <pant.lalit@gmail.com>
2012-12-01 09:35:33 +0530

    Tweak history search.

--- src/main/scala/net/kogics/kojo/lite/topc/HistoryHolder.scala
+++ src/main/scala/net/kogics/kojo/lite/topc/HistoryHolder.scala
@@ -1,7 +1,17 @@
 package net.kogics.kojo.lite.topc
 
 import javax.swing.JComponent
+import bibliothek.gui.dock.common.event.CFocusListener
+import bibliothek.gui.dock.common.intern.CDockable
+import net.kogics.kojo.history.HistoryPanel
 
-class HistoryHolder(val hw: JComponent) extends BaseHolder("HW", "History Pane", hw) {
+class HistoryHolder(val hw: HistoryPanel) extends BaseHolder("HW", "History Pane", hw) {
+  addFocusListener(new CFocusListener {
+    override def focusGained(dockable: CDockable) {
+      hw.searchField.requestFocusInWindow()
+    }
 
+    override def focusLost(dockable: CDockable) {
+    }
+  })
 }
