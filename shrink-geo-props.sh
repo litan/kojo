446c63805c3ea65b7dc44010a812c70f8b9ac0f4
Lalit Pant <pant.lalit@gmail.com>
2012-12-14 16:34:14 +0530

    Retain swedish prop files in mathworld.

--- shrink-geo-props.sh
+++ shrink-geo-props.sh
@@ -1,1 +1,21 @@
-zip -d lib/geogebra_properties.jar geogebra/properties/*_*.properties
+cd lib
+mv geogebra_properties.jar xgeogebra_properties.jar
+unzip xgeogebra_properties.jar
+zip geogebra_properties.jar geogebra/properties/colors.properties
+zip geogebra_properties.jar geogebra/properties/colors_sv.properties
+zip geogebra_properties.jar geogebra/properties/command.properties
+zip geogebra_properties.jar geogebra/properties/command_sv.properties
+zip geogebra_properties.jar geogebra/properties/error.properties
+zip geogebra_properties.jar geogebra/properties/error_sv.properties
+zip geogebra_properties.jar geogebra/properties/javaui.properties
+zip geogebra_properties.jar geogebra/properties/javaui_sv.properties
+zip geogebra_properties.jar geogebra/properties/menu.properties
+zip geogebra_properties.jar geogebra/properties/menu_sv.properties
+zip geogebra_properties.jar geogebra/properties/plain.properties
+zip geogebra_properties.jar geogebra/properties/plain_sv.properties
+zip geogebra_properties.jar geogebra/properties/symbols.properties
+zip geogebra_properties.jar geogebra/properties/symbols_sv.properties
+rm xgeogebra_properties.jar
+rm -rf geogebra
+rm -rf META-INF
+cd ..
