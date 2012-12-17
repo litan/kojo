448046c5cc6aaa291dcd083ed5a3e913743d37bd
Lalit Pant <lalit_pant@yahoo.com>
2012-12-17 16:29:40 +0530

    Formatting and import reorg.

--- src/main/scala/net/kogics/kojo/xscala/Builtins.scala
+++ src/main/scala/net/kogics/kojo/xscala/Builtins.scala
@@ -16,22 +16,23 @@
 package net.kogics.kojo
 package xscala
 
-import java.awt.{Color => JColor}
+import java.awt.{ Color => JColor }
 import java.awt.GradientPaint
 import java.awt.Paint
 
-import core.InitedSingleton
+import javax.swing.JComponent
+
+import net.kogics.kojo.core.InitedSingleton
+import net.kogics.kojo.lite.CodeExecutionSupport
+import net.kogics.kojo.story.HandlerHolder
+import net.kogics.kojo.util.Read
+
 import core.Rectangle
 import core.Style
 import core.TSCanvasFeatures
 import core.TurtleMover
 import core.UnitLen
 import core.Voice
-import javax.swing.JComponent
-import net.kogics.kojo.core.InitedSingleton
-import net.kogics.kojo.lite.CodeExecutionSupport
-import net.kogics.kojo.story.HandlerHolder
-import net.kogics.kojo.util.Read
 import story.HandlerHolder
 import story.IntHandlerHolder
 import story.StringHandlerHolder
@@ -66,8 +67,8 @@ class Builtins extends RepeatCommands {
   type PolyLine = kgeom.PolyLine
   val PolyLine = kgeom.PolyLine
   type Point2D = java.awt.geom.Point2D.Double
-  def Point2D(x: Double, y: Double) = new java.awt.geom.Point2D.Double(x,y)
-  
+  def Point2D(x: Double, y: Double) = new java.awt.geom.Point2D.Double(x, y)
+
   val Random = new java.util.Random
 
   val blue = JColor.blue
@@ -88,7 +89,7 @@ class Builtins extends RepeatCommands {
 
   val C = staging.KColor
   val noColor = C.noColor
-  
+
   val Kc = new staging.KeyCodes
 
   // Turtle World
@@ -96,7 +97,7 @@ class Builtins extends RepeatCommands {
     def forward() = println("Please provide the distance to move forward - e.g. forward(100)")
     override def forward(n: Double) = turtle0.forward(n)
     UserCommand("forward", List("numSteps"), "Moves the turtle forward a given number of steps.")
-    
+
     def back() = println("Please provide the distance to move back - e.g. back(100)")
     override def back(n: Double) = turtle0.back(n)
     UserCommand("back", List("numSteps"), "Moves the turtle back a given number of steps.")
@@ -219,10 +220,10 @@ class Builtins extends RepeatCommands {
   // Define class and then define value - to get around:
   // Problem finding completions: assertion failed: fatal: <refinement> has non-class owner value Tw after flatten.
   val Tw = new TwC()
-  
+
   // Turtle and Staging Canvas
   class TSCanvasC extends TSCanvasFeatures {
-    
+
     override def zoom(factor: Double) = tCanvas.zoom(factor)
     override def zoom(factor: Double, cx: Double, cy: Double) = tCanvas.zoom(factor, cx, cy)
     UserCommand("zoom", List("factor"), "Zooms in by the given factor, leaving the center point unchanged.")
@@ -251,10 +252,10 @@ class Builtins extends RepeatCommands {
     override def exportThumbnail(filePrefix: String, height: Int) = tCanvas.exportThumbnail(filePrefix, height)
     override def zoomXY(xfactor: Double, yfactor: Double, cx: Double, cy: Double) =
       tCanvas.zoomXY(xfactor, yfactor, cx, cy)
-    
+
     def onKeyPress(fn: Int => Unit) = tCanvas.onKeyPress(fn)
     def onKeyRelease(fn: Int => Unit) = tCanvas.onKeyRelease(fn)
-    def onMouseClick(fn: (Double, Double) => Unit) =  tCanvas.onMouseClick(fn)
+    def onMouseClick(fn: (Double, Double) => Unit) = tCanvas.onMouseClick(fn)
 
     val Pixel = core.Pixel
     val Inch = core.Inch
@@ -295,12 +296,12 @@ class Builtins extends RepeatCommands {
 
   def print(obj: Any) {
     // Runs on Actor pool (interpreter) thread
-    scalaCodeRunner.kprintln("%s" format(obj))
+    scalaCodeRunner.kprintln("%s" format (obj))
     Throttler.throttle()
   }
   UserCommand.addCompletion("print", List("obj"))
 
-  def println(obj: Any): Unit = print("%s\n" format(obj))
+  def println(obj: Any): Unit = print("%s\n" format (obj))
   UserCommand.addCompletion("println", List("obj"))
   UserCommand.addSynopsis("println(obj) or print(obj) - Displays the given object as a string in the output window.")
 
@@ -321,7 +322,7 @@ class Builtins extends RepeatCommands {
 
   def color(r: Int, g: Int, b: Int) = new Color(r, g, b)
   UserCommand("color", List("red", "green", "blue"), "Creates a new color based on the specified red, green, and blue levels.")
-  
+
   def setAstStopPhase(phase: String): Unit = astStopPhase = phase
   UserCommand.addSynopsis("astStopPhase - Gets the compiler phase value for AST printing.")
   UserCommand("setAstStopPhase", List("stopBeforePhase"), "Sets the compiler phase value for AST printing.")
@@ -349,16 +350,11 @@ class Builtins extends RepeatCommands {
   UserCommand("stPlayStory", List("story"), "Play the given story.")
 
   def stFormula(latex: String, size: Int = 18, cssColor: String = null) =
-    <div style={"text-align:center;margin:6px;"}>
-      {if (cssColor != null) {
-          <img src={xml.Unparsed(story.CustomHtmlEditorKit.latexPrefix + latex)}
-            style={"color:%s" format(cssColor)}
-            height={"%d" format(size)} />
-        }
-        else {
-          <img src={xml.Unparsed(story.CustomHtmlEditorKit.latexPrefix + latex)}
-            height={"%d" format(size)} />
-        }}
+    <div style={ "text-align:center;margin:6px;" }>
+      { if (cssColor != null) { <img src={ xml.Unparsed(story.CustomHtmlEditorKit.latexPrefix + latex) }
+            style={ "color:%s" format(cssColor) }
+            height={ "%d" format(size) } /> } else { <img src={ xml.Unparsed(story.CustomHtmlEditorKit.latexPrefix + latex) }
+            height={ "%d" format(size) } /> } }
     </div>
   UserCommand("stFormula", List("latex"), "Converts the supplied latex string into html that can be displayed in the Story Teller Window.")
 
@@ -387,7 +383,7 @@ class Builtins extends RepeatCommands {
   implicit val DoubleRead = util.Read.DoubleRead
   implicit val IntRead = util.Read.IntRead
   import util.Read
-  
+
   def stFieldValue[T](label: String, default: T)(implicit reader: Read[T]): T = {
     storyTeller.fieldValue(label, default)
   }
@@ -419,7 +415,7 @@ class Builtins extends RepeatCommands {
   }
   UserCommand("stNext", Nil, "Moves the story to the next page/view.")
   UserCommand.addSynopsisSeparator()
-  
+
   def help() = {
     println("""You can press Ctrl-Space in the script window at any time to see available commands and functions.
 
@@ -472,17 +468,17 @@ Here's a partial list of the available commands:
   def interpret(code: String) {
     scalaCodeRunner.runCode(code)
   }
-  
+
   // for debugging only
   def kojoInterp = scalaCodeRunner.kojointerp
   def pcompiler = scalaCodeRunner.pcompiler
-  
-  def reimportBuiltins() { 
+
+  def reimportBuiltins() {
     interpret("import TSCanvas._; import Tw._")
   }
   def reimportDefaults() = reimportBuiltins()
-  
-  import story.{HandlerHolder, IntHandlerHolder, StringHandlerHolder, VoidHandlerHolder}
+
+  import story.{ HandlerHolder, IntHandlerHolder, StringHandlerHolder, VoidHandlerHolder }
   implicit def toIhm(handler: Int => Unit): HandlerHolder[Int] = new IntHandlerHolder(handler)
   implicit def toShm(handler: String => Unit): HandlerHolder[String] = new StringHandlerHolder(handler)
   implicit def toVhm(handler: () => Unit): HandlerHolder[Unit] = new VoidHandlerHolder(handler)
@@ -490,7 +486,7 @@ Here's a partial list of the available commands:
   def stAddLinkHandler[T](name: String, story: Story)(implicit hm: HandlerHolder[T]) {
     storyTeller.addLinkHandler(name, story)(hm)
   }
-  
+
   def stAddUiComponent(c: JComponent) {
     storyTeller.addUiComponent(c)
   }
@@ -499,13 +495,13 @@ Here's a partial list of the available commands:
   def stGotoUrl(url: String) = Utils.runInSwingThread {
     urlHandler.gotoUrl(new java.net.URL(url))
   }
-  
+
   type Picture = core.Picture
   type Painter = picture.Painter
   type Pic = picture.Pic
-//  val Pic = picture.Pic
+  //  val Pic = picture.Pic
   type Pic0 = picture.Pic0
-//  val Pic0 = picture.Pic0
+  //  val Pic0 = picture.Pic0
   type HPics = picture.HPics
   val HPics = picture.HPics
   type VPics = picture.VPics
@@ -513,32 +509,32 @@ Here's a partial list of the available commands:
   type GPics = picture.GPics
   val GPics = picture.GPics
 
-//  type Rot = picture.Rot
-//  val Rot = picture.Rot
-//  type Rotp = picture.Rotp
-//  val Rotp = picture.Rotp
-//  type Scale = picture.Scale
-//  val Scale = picture.Scale
-//  type Opac = picture.Opac
-//  val Opac = picture.Opac
-//  type Hue = picture.Hue
-//  val Hue = picture.Hue
-//  type Sat = picture.Sat
-//  val Sat = picture.Sat
-//  type Brit = picture.Brit
-//  val Brit = picture.Brit
-//  type Trans = picture.Trans
-//  val Trans = picture.Trans
-//  type FlipX = picture.FlipX
-//  val FlipX = picture.FlipX
-//  type FlipY = picture.FlipY
-//  val FlipY = picture.FlipY
-//  type Fill = picture.Fill
-//  val FillColor = picture.Fill
-//  type Stroke = picture.Stroke
-//  val PenColor = picture.Stroke
-//  type StrokeWidth = picture.StrokeWidth
-//  val PenWidth = picture.StrokeWidth
+  //  type Rot = picture.Rot
+  //  val Rot = picture.Rot
+  //  type Rotp = picture.Rotp
+  //  val Rotp = picture.Rotp
+  //  type Scale = picture.Scale
+  //  val Scale = picture.Scale
+  //  type Opac = picture.Opac
+  //  val Opac = picture.Opac
+  //  type Hue = picture.Hue
+  //  val Hue = picture.Hue
+  //  type Sat = picture.Sat
+  //  val Sat = picture.Sat
+  //  type Brit = picture.Brit
+  //  val Brit = picture.Brit
+  //  type Trans = picture.Trans
+  //  val Trans = picture.Trans
+  //  type FlipX = picture.FlipX
+  //  val FlipX = picture.FlipX
+  //  type FlipY = picture.FlipY
+  //  val FlipY = picture.FlipY
+  //  type Fill = picture.Fill
+  //  val FillColor = picture.Fill
+  //  type Stroke = picture.Stroke
+  //  val PenColor = picture.Stroke
+  //  type StrokeWidth = picture.StrokeWidth
+  //  val PenWidth = picture.StrokeWidth
   val rot = picture.rot _
   val rotp = picture.rotp _
   def scale(f: Double) = picture.scale(f)
@@ -557,12 +553,12 @@ Here's a partial list of the available commands:
   val penColor = picture.stroke _
   val penWidth = picture.strokeWidth _
   val deco = picture.deco _
-  
-//  type Spin = picture.Spin
-//  val Spin = picture.Spin
+
+  //  type Spin = picture.Spin
+  //  val Spin = picture.Spin
   val spin = picture.spin _
-//  type Reflect = picture.Reflect
-//  val Reflect = picture.Reflect
+  //  type Reflect = picture.Reflect
+  //  val Reflect = picture.Reflect
   val reflect = picture.reflect _
   val row = picture.row _
   val col = picture.col _
@@ -572,32 +568,32 @@ Here's a partial list of the available commands:
   def Picture(fn: => Unit) = picture.Pic0 { t =>
     fn
   }
-  def draw(pictures: Picture *) = pictures.foreach {_ draw()}
-  def drawAndHide(pictures: Picture *) = pictures.foreach {p => p.draw(); p.invisible()}
-  def show(pictures: Picture *) {
+  def draw(pictures: Picture*) = pictures.foreach { _ draw () }
+  def drawAndHide(pictures: Picture*) = pictures.foreach { p => p.draw(); p.invisible() }
+  def show(pictures: Picture*) {
     throw new UnsupportedOperationException("Use draw(pic/s) instead of show(pic/s)")
   }
-  
+
   def setRefreshRate(fps: Int) {
-    require(fps >= 20 && fps <=100, "FPS needs to be in the range: 20 to 100")
+    require(fps >= 20 && fps <= 100, "FPS needs to be in the range: 20 to 100")
     kojoCtx.fps = fps
   }
   def animate(fn: => Unit) = staging.API.loop(fn)
   def stopAnimation() = ctx.stopAnimation()
   def isKeyPressed(key: Int) = staging.Inputs.isKeyPressed(key)
   def activateCanvas() = tCanvas.activate()
-  def Color(r: Int, g: Int, b: Int, a: Int=255) = new Color(r, g, b, a)
+  def Color(r: Int, g: Int, b: Int, a: Int = 255) = new Color(r, g, b, a)
   def ColorG(x1: Double, y1: Double, c1: Color, x2: Double, y2: Double, c2: Color, cyclic: Boolean = false) = {
     new GradientPaint(x1.toFloat, y1.toFloat, c1, x2.toFloat, y2.toFloat, c2, cyclic)
   }
-  def ColorHSB(h: Double, s: Double, b: Double) = java.awt.Color.getHSBColor((h/360).toFloat, (s/100).toFloat, (b/100).toFloat)
+  def ColorHSB(h: Double, s: Double, b: Double) = java.awt.Color.getHSBColor((h / 360).toFloat, (s / 100).toFloat, (b / 100).toFloat)
   val hueMod = Utils.hueMod _
   val satMod = Utils.satMod _
   val britMod = Utils.britMod _
-  
+
   type Vector2D = util.Vector2D
   val Vector2D = util.Vector2D
-  
+
   val mp3player = music.KMp3.instance
   def playMp3(mp3File: String) {
     mp3player.playMp3(mp3File)
@@ -608,11 +604,11 @@ Here's a partial list of the available commands:
     mp3player.playMp3Loop(mp3File)
   }
   UserCommand("playMp3Loop", List("fileName"), "Plays the specified MP3 file in the background.")
-  
+
   def installDir = Utils.installDir
   def canvasBounds = tCanvas.cbounds
   def setBackground(c: Paint) = tCanvas.setCanvasBackground(c)
-  
+
   def isMp3Playing = music.KMp3.instance.isMusicPlaying
   def isMusicPlaying = music.FuguePlayer.instance.isMusicPlaying
   def stopMp3() = music.KMp3.instance.stopMp3()
@@ -625,21 +621,21 @@ Here's a partial list of the available commands:
   def addHelpContent(lang: String, content: Map[String, String]) {
     Help.addContent(lang, content)
   }
-  
+
   def bounceVecOffStage(v: Vector2D, p: Picture) {
     import TSCanvas._
     val stageparts = List(stageTop, stageBot, stageLeft, stageRight)
     p.collision(stageparts).foreach {
       _ match {
-        case p if p == stageTop => v.update(v.x, -v.y)
-        case p if p == stageBot => v.update(v.x, -v.y)
-        case p if p == stageLeft => v.update(-v.x, v.y)
+        case p if p == stageTop   => v.update(v.x, -v.y)
+        case p if p == stageBot   => v.update(v.x, -v.y)
+        case p if p == stageLeft  => v.update(-v.x, v.y)
         case p if p == stageRight => v.update(-v.x, v.y)
-        case _ =>
+        case _                    =>
       }
     }
   }
-  
+
   def switchToDefaultPerspective() = Utils.runInSwingThreadAndWait {
     kojoCtx.switchToDefaultPerspective()
   }
