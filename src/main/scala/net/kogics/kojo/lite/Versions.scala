a8415385187ffa58c066abd8072182215c84e34f
Lalit Pant <pant.lalit@gmail.com>
2013-01-29 22:45:24 +0530

    Load IP story from local storage instead of over the web.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -1,7 +1,7 @@
 package net.kogics.kojo.lite
 
 object Versions {
-    val KojoVersion = "280113-1"
+    val KojoVersion = "290113-1"
     val JavaVersion = geogebra.main.AppD.getJavaVersion
     val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
