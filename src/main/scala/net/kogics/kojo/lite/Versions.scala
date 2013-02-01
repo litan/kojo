d5ced1ba701d4ab550704cbae62957c860cea32e
Lalit Pant <lalit_pant@yahoo.com>
2013-02-01 08:52:01 +0530

    Bump up build number to 310113-1

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -1,7 +1,7 @@
 package net.kogics.kojo.lite
 
 object Versions {
-    val KojoVersion = "300113-1"
+    val KojoVersion = "310113-1"
     val JavaVersion = geogebra.main.AppD.getJavaVersion
     val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
