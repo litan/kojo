fc4adfba5e34bdca3e973904c53078e91d0e3b4e
Lalit Pant <pant.lalit@gmail.com>
2013-02-13 07:56:19 +0530

    Bump up version number.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -1,7 +1,7 @@
 package net.kogics.kojo.lite
 
 object Versions {
-    val KojoVersion = "120213-1"
+    val KojoVersion = "120213-2"
     val JavaVersion = geogebra.main.AppD.getJavaVersion
     val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
