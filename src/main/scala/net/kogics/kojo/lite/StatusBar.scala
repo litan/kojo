cf08d92e3fb3103f89b454a17fdfbc86720d63a9
Lalit Pant <lalit_pant@yahoo.com>
2013-02-21 09:19:59 +0530

    Add script editor line/col information to Statusbar.

--- src/main/scala/net/kogics/kojo/lite/StatusBar.scala
+++ src/main/scala/net/kogics/kojo/lite/StatusBar.scala
@@ -1,19 +1,30 @@
 package net.kogics.kojo.lite
 
-import java.awt.FlowLayout
+import java.awt.BorderLayout
+import java.awt.Color
 import java.awt.Font
+
+import javax.swing.BorderFactory
 import javax.swing.JLabel
 import javax.swing.JPanel
-import java.awt.Dimension
 
 class StatusBar extends JPanel {
-  setLayout(new FlowLayout(FlowLayout.LEFT, 5, 0))
-  setPreferredSize(new Dimension(0, 16))
+  setLayout(new BorderLayout())
   val statusText = new JLabel
   statusText.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 11))
-  add(statusText)
+  statusText.setBorder(BorderFactory.createLineBorder(Color.lightGray))
+  add(statusText, BorderLayout.CENTER)
+
+  val lineCol = new JLabel
+  lineCol.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12))
+  lineCol.setBorder(BorderFactory.createLineBorder(Color.lightGray))
+  add(lineCol, BorderLayout.EAST)
 
   def showText(text: String) {
     statusText.setText(text)
+  }
+
+  def showCaretPos(line: Int, col: Int) {
+    lineCol.setText(s"${" " * 12}$line | $col${" " * 12}")
   }
 }
