096f0a69a7ed40e66450202738aadf5293309f8c
Lalit Pant <lalit_pant@yahoo.com>
2013-03-01 19:24:44 +0530

    Rename changeCostume to setCostume.

--- src/main/scala/net/kogics/kojo/turtle/TurtleWorldAPI.scala
+++ src/main/scala/net/kogics/kojo/turtle/TurtleWorldAPI.scala
@@ -115,5 +115,6 @@ class TurtleWorldAPI(turtle0: => core.Turtle) extends TurtleMover {
     def style: Style = turtle0.style
 
     def arc(r: Double, a: Int) = turtle0.arc(r, a)
-    def changeCostume(costume: String) = turtle0.changeCostume(costume)
+    def setCostume(costumeFile: String) = turtle0.setCostume(costumeFile)
+    UserCommand("setCostume", List("costumeFile"), "Changes the costume (i.e. image) associated with the turtle to the image in the specified file.")
 }
