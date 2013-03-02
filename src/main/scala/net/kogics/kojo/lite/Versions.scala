7a6f039e7efe8b211fa1642d04b8ff153da3fae6
Lalit Pant <pant.lalit@gmail.com>
2013-03-02 14:40:08 +0530

    * Add Swedish translations for new samples. * Allow newTurtle call without parens (for backward compatibility). * Add parens in newTurtle call in the Swedish init code (newTurtle is a kind of a command).

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,7 +2,7 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.1 Beta"
-  val KojoVersion = "020313-1"
+  val KojoVersion = "020313-2"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
