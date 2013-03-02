3ac952f13e55403832314c915209f2dcbf1d4a4c
Lalit Pant <lalit_pant@yahoo.com>
2013-03-02 20:34:29 +0530

    Remove version numbers from (most) installed jars - to make upgrades easier.

--- installer/cp-staging-jars.scala
+++ installer/cp-staging-jars.scala
@@ -1,7 +1,11 @@
 import io.Source
 import java.io.File
 import sys.process.stringToProcess
+
 val jars = Source.fromFile("/home/lalit/work/KojoDev/KojoLite/installer/jarlist.txt")
 jars.getLines.foreach { j =>
-    s"cp -v $j /home/lalit/work/KojoDev/KojoLite/installerbuild/lib/"!
+  val srcDest = j split " -> "
+  val src = srcDest(0)
+  val dest = if (srcDest.size > 1) srcDest(1) else ""
+  s"cp -v $src /home/lalit/work/KojoDev/KojoLite/installerbuild/lib/$dest"!
 }
