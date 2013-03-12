e5f677ad34ece6162f0f740162a54a8975f2203b
Lalit Pant <lalit_pant@yahoo.com>
2013-03-12 17:54:00 +0530

    Update turtle postion as it moves forward; and related refactoring.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,7 +2,7 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.1 Beta"
-  val KojoVersion = "120313-1"
+  val KojoVersion = "120313-2"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
