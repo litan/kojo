6108da0cc1d0ee566a4a2dc2de8f02d70424a0f8
Lalit Pant <lalit_pant@yahoo.com>
2013-08-22 10:51:36 +0530

    Convert <void value> to Unit in returns.

--- src/main/scala/net/kogics/kojo/lite/trace/MethodEvent.scala
+++ src/main/scala/net/kogics/kojo/lite/trace/MethodEvent.scala
@@ -31,13 +31,14 @@ class MethodEvent {
   var callerLine: String = _
 
   def pargs = args.mkString("(", ", ", ")")
+  def pret = returnVal.replaceAllLiterally("<void value>", "Unit")
 
   def entry(level: Int) = {
     <html><div style="font-family:Monospace"><span style="color:rgb(0,50,225)">{ "> " * level } [Enter]</span> { methodName }<span style="color:rgb(0,50,225)">{ pargs }</span></div></html>.toString
   }
 
   def exit(level: Int) = {
-    <html><div style="font-family:Monospace"><span style="color:rgb(225,50,0)">{ "< " * level } [Exit]</span> { methodName } <span style="color:rgb(225,50,0)">[{ returnVal }]</span></div></html>.toString
+    <html><div style="font-family:Monospace"><span style="color:rgb(225,50,0)">{ "< " * level } [Exit]</span> { methodName } <span style="color:rgb(225,50,0)">[{ pret }]</span></div></html>.toString
   }
 
   override def toString() = {
