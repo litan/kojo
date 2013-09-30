53211f8e27802473aabb89217d869057cf34359a
Lalit Pant <lalit_pant@yahoo.com>
2013-09-30 11:37:02 +0530

    Enable installer building from any workspace directory.

--- installer/cp-staging-jars.scala
+++ installer/cp-staging-jars.scala
@@ -2,10 +2,10 @@ import io.Source
 import java.io.File
 import sys.process.stringToProcess
 
-val jars = Source.fromFile("/home/lalit/work/KojoDev/KojoLite/installer/jarlist.txt")
+val jars = Source.fromFile("./jarlist.txt")
 jars.getLines.foreach { j =>
   val srcDest = j split " -> "
   val src = srcDest(0)
   val dest = if (srcDest.size > 1) srcDest(1) else ""
-  s"cp -v $src /home/lalit/work/KojoDev/KojoLite/installerbuild/lib/$dest"!
+  s"cp -v $src ../installerbuild/lib/$dest"!
 }
