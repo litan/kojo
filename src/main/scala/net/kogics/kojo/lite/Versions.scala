5e472b6e1e46eb1b44ad64a6277472db4aa7cc2a
Lalit Pant <lalit_pant@yahoo.com>
2013-10-16 17:27:09 +0530

    Fix readInput() behavior for a forced stop in the middle of input reading.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,7 +2,7 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.1 Beta"
-  val KojoVersion = "151013-1"
+  val KojoVersion = "161013-1"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
