1ed583d96e786bd40aa0861a736a4532225f3995
Lalit Pant <lalit_pant@yahoo.com>
2014-01-20 20:37:52 +0530

    Play with fixing Instruction Palette issue (which I can't reproduce) reported by Wayne.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,7 +2,7 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.1 Beta"
-  val KojoVersion = "151213-1"
+  val KojoVersion = "200114-2"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
