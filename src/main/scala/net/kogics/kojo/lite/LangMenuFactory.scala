9b490260809e64c9254066d5604cee83e1a70cb6
ezoerner <ezoerner@zion.lan>
2014-05-03 20:34:07 +0200

    Added icons to the Language menu and refactored AppMenu to extract out a LangMenuFactory

---
+++ src/main/scala/net/kogics/kojo/lite/LangMenuFactory.scala
@@ -0,0 +1,76 @@
+package net.kogics.kojo.lite
+
+import javax.swing.{JMenu, JOptionPane, JCheckBoxMenuItem, ImageIcon}
+import net.kogics.kojo.core
+import java.awt.event.{ActionEvent, ActionListener}
+import net.kogics.kojo.util.Utils
+
+/**
+ * Creates the Language menu.
+ *
+ * @author Eric Zoerner <a href="mailto:eric.zoerner@gmail.com">eric.zoerner@gmail.com</a>
+ */
+object LangMenuFactory {
+
+  val supportedLanguages = List("en", "sv", "fr", "pl" /*, "nl", "it", "hi" */)
+
+  def createLangMenu()(implicit kojoCtx: core.KojoCtx) = {
+    var langMenus: Seq[JCheckBoxMenuItem] = Vector()
+    val langHandler = new ActionListener {
+      override def actionPerformed(e: ActionEvent) {
+        val lang = e.getActionCommand
+        kojoCtx.userLanguage = lang
+        langMenus foreach {
+          mi =>
+            if (mi.getActionCommand != lang) {
+              mi.setSelected(false)
+            }
+        }
+        JOptionPane.showMessageDialog(kojoCtx.frame,
+                                      Utils.loadString("S_LangChanged").format(e.getSource.asInstanceOf[JCheckBoxMenuItem].getText),
+                                      Utils.loadString("S_LangChange"),
+                                      JOptionPane.INFORMATION_MESSAGE)
+      }
+    }
+
+    def langMenuItem(langCode: String) = {
+      val langName = langNames(langCode)
+      val mitem = new JCheckBoxMenuItem(langName)
+      mitem.addActionListener(langHandler)
+      mitem.setActionCommand(langCode)
+      mitem.setIcon(langIcon(langCode))
+      if (kojoCtx.userLanguage == langCode) {
+        mitem.setSelected(true)
+      }
+      langMenus :+= mitem
+      mitem
+    }
+
+    val langMenu = new JMenu(Utils.loadString("S_Language"))
+    langMenu.setMnemonic('L')
+    langMenu.setIcon(langIcon(kojoCtx.userLanguage))
+    supportedLanguages.foreach {lang ⇒ langMenu.add(langMenuItem(lang))}
+    langMenu
+  }
+
+  private val langNames = Map (
+    "en" → "English",
+    "sv" → "Svenska (Swedish)",
+    "fr" → "Français (French)",
+    "pl" → "Polski (Polish)",
+    "nl" → "Nederlands (Dutch)",
+    "it" → "Italiano (Italian)",
+    "hi" → "हिंदी (Hindi)"
+  )
+
+  private val langToCountry = Map(
+    "en" → "gb",
+    "hi" → "in"
+  )
+
+  private def langIcon(langCode: String): ImageIcon = {
+    val countryCode = langToCountry.get(langCode).getOrElse(langCode)
+    val iconUrl = getClass.getClassLoader.getResource(s"geogebra/gui/menubar/images/$countryCode.png")
+    new ImageIcon(iconUrl)
+  }
+}
