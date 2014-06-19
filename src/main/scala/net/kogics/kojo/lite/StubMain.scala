d2d27ecf6d121cf557ad68b985eef3580942146e
Lalit Pant <lalit_pant@yahoo.com>
2014-06-19 12:51:21 +0530

    Set java.library.path to user libk dir without appending to earlier java.library.path (similar in spirit to ignoring CLASSPATH env var).

--- src/main/scala/net/kogics/kojo/lite/StubMain.scala
+++ src/main/scala/net/kogics/kojo/lite/StubMain.scala
@@ -69,11 +69,10 @@ trait StubMain {
       "-Xss1m -XX:PermSize=32m -XX:MaxPermSize=256m -Dapple.laf.useScreenMenuBar=true " +
       "-Dapple.awt.graphics.UseQuartz=true -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled " +
       "net.kogics.kojo.lite.Main %s" format (args.mkString(" "))
-    val libPath = System.getProperty("java.library.path").replaceAllLiterally("\"", "")
     val commandSeq =
       Seq(javaExec,
         "-cp", classpath,
-        s"-Djava.library.path=${libPath}${File.pathSeparator}${Utils.libDir}"
+        s"-Djava.library.path=${Utils.libDir}"
       ) ++ cmdPart.split(' ')
 
     commandSeq!
