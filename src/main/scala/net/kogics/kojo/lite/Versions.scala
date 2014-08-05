a4962e42d26a2e928327dd76d8aa90823727e481
Lalit Pant <pant.lalit@gmail.com>
2014-08-05 13:02:27 +0530

    Upgrade to Scala 2.11.2. Also change Kojo version number to 2.4.01 (and move away from MMDDYY version format) to support packaging on linux with ever increasing version numbers.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -1,8 +1,8 @@
 package net.kogics.kojo.lite
 
 object Versions {
-  val KojoMajorVersion = "2.1 Beta"
-  val KojoVersion = "190614-2"
+  val KojoMajorVersion = "2.4 Beta"
+  val KojoVersion = "2.4.01"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
