648c9808b493ed00561dfe99b5663312635642ba
Lalit Pant <lalit_pant@yahoo.com>
2014-08-07 10:05:46 +0530

    Add a revision number to the new Kojo versioning scheme to enable non (desktop) release version incrementing.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,6 +3,7 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.01"
+  val KojoRevision = "r1"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
