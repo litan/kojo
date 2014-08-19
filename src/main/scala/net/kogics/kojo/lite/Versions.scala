1d5a9d6d688c8c2633d2a7c76322cf67702989e8
Lalit Pant <lalit_pant@yahoo.com>
2014-08-19 23:04:46 +0530

    Incorporate translations for the 'toggle menubar' item in the script editor context menu.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,7 +3,7 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.01"
-  val KojoRevision = "r5"
+  val KojoRevision = "r6"
   val KojoBuildDate = "19 Aug 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
