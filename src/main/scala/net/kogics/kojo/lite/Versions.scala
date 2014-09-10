9d632c5c513c80f35cad0050814daba2af577a9b
Lalit Pant <pant.lalit@gmail.com>
2014-09-10 21:55:15 +0530

    Fix tracing of drawings with a  group of pictures (HPics, GPics, etc).

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,9 +2,9 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
-  val KojoVersion = "2.4.01"
-  val KojoRevision = "r7"
-  val KojoBuildDate = "21 Aug 2014"
+  val KojoVersion = "2.4.02"
+  val KojoRevision = "r1"
+  val KojoBuildDate = "10 Sep 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
