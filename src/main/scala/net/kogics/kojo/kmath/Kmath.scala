2a6802c4b18ecfbde8b3962030352380fe6c0fb5
Lalit Pant <pant.lalit@gmail.com>
2014-09-16 09:48:48 +0530

    Add hcf and lcm functions to Kmath.

--- src/main/scala/net/kogics/kojo/kmath/Kmath.scala
+++ src/main/scala/net/kogics/kojo/kmath/Kmath.scala
@@ -4,6 +4,7 @@ package kmath
 import scala.language.implicitConversions
 
 import org.apache.commons.math.stat.StatUtils
+import org.apache.commons.math.util.MathUtils
 
 object Kmath {
   implicit def seqToArrD(seq: Seq[Double]): Array[Double] = seq.toArray
@@ -16,6 +17,13 @@ object Kmath {
   def constrain(value: Double, min: Double, max: Double) = util.Math.constrain(value, min, max)
   def map(value: Double, low1: Double, high1: Double, low2: Double, high2: Double) = util.Math.map(value, low1, high1, low2, high2)
   def lerp(value1: Double, value2: Double, amt: Double) = util.Math.lerp(value1, value2, amt)
+  
+  def gcd(p: Int, q: Int) = MathUtils.gcd(p, q)
+  def gcd(p: Long, q: Long) = MathUtils.gcd(p, q)
+  def hcf(p: Int, q: Int) = MathUtils.gcd(p, q)
+  def hcf(p: Long, q: Long) = MathUtils.gcd(p, q)
+  def lcm(p: Int, q: Int) = MathUtils.lcm(p, q)
+  def lcm(p: Long, q: Long) = MathUtils.lcm(p, q)
 }
 
 
