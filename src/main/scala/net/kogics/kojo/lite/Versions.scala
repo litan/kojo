fbca2e745531a40cd76994653d7c7c229f89206a
Lalit Pant <lalit_pant@yahoo.com>
2014-09-20 18:48:35 +0530

    Start Kojo maximized on Windows (after the recent frame.setBounds change).

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.02"
-  val KojoRevision = "r4"
-  val KojoBuildDate = "16 Sep 2014"
+  val KojoRevision = "r5"
+  val KojoBuildDate = "20 Sep 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
