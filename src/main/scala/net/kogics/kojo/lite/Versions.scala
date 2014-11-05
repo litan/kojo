fa4ddd5a963562b608e0f3bd662b18371defe375
Lalit Pant <pant.lalit@gmail.com>
2014-11-05 23:40:20 +0530

    Add availableFonts function.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,9 +2,9 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
-  val KojoVersion = "2.4.03"
-  val KojoRevision = "r6"
-  val KojoBuildDate = "25 Oct 2014"
+  val KojoVersion = "2.4.04"
+  val KojoRevision = "r1"
+  val KojoBuildDate = "5 Nov 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
