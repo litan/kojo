57a4a9f8f9ac18a4a151a2410691e010e061fb4c
Lalit Pant <lalit_pant@yahoo.com>
2014-11-12 23:24:42 +0530

    Put a bound on the GUI work transfer queue -- to enable stopping scripts that generate *big* drawings without any animation delay.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.04"
-  val KojoRevision = "r3"
-  val KojoBuildDate = "7 Nov 2014"
+  val KojoRevision = "r4"
+  val KojoBuildDate = "12 Nov 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
