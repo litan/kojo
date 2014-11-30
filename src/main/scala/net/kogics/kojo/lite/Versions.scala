67638ded1cd4f3c2dd72eeff5945c9a2e2e43099
Lalit Pant <lalit_pant@yahoo.com>
2014-11-30 13:06:45 +0530

    Create builtin type aliases for widgets to allow convenient customization in scripts.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.04"
-  val KojoRevision = "r10"
-  val KojoBuildDate = "29 Nov 2014"
+  val KojoRevision = "r11"
+  val KojoBuildDate = "30 Nov 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
