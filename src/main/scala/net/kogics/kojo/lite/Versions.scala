9c881de19610af3b111adbd73d9d3b1ceec8939b
Lalit Pant <pant.lalit@gmail.com>
2014-12-08 22:50:44 +0530

    Make challenge runner show turtle when first step/line is blank.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,7 +3,7 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.04"
-  val KojoRevision = "r17"
+  val KojoRevision = "r18"
   val KojoBuildDate = "8 Dec 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
