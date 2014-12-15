1cf4f58e6e2bb442a77836b7a4ab6e6e4e243b5b
Lalit Pant <pant.lalit@gmail.com>
2014-12-15 10:21:46 +0530

    Enable chaining of commands for logo-like turtle; plus misc enhancements.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.04"
-  val KojoRevision = "r21"
-  val KojoBuildDate = "14 Dec 2014"
+  val KojoRevision = "r22"
+  val KojoBuildDate = "15 Dec 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
