3f54b4cb8d93e789831305aa846960b85cc330bf
Lalit Pant <lalit_pant@yahoo.com>
2014-12-16 20:40:46 +0530

    Make types even more dymanic, to allow definition of new commands that can take Any params (at the cost of static, line-specific, error reporting).

--- src/main/scala/net/kogics/kojo/turtle/LoTurtle.scala
+++ src/main/scala/net/kogics/kojo/turtle/LoTurtle.scala
@@ -10,39 +10,45 @@ import net.kogics.kojo.kmath.Kmath
 
 class LoTurtle(val t: core.Turtle) extends Dynamic {
   lazy val cmdsu = collection.mutable.HashMap.empty[Symbol, Unit => LoTurtle]
-  lazy val cmdsd = collection.mutable.HashMap.empty[Symbol, Double => LoTurtle]
-  lazy val cmdsdd = collection.mutable.HashMap.empty[Symbol, (Double, Double) => LoTurtle]
-  lazy val cmdsddd = collection.mutable.HashMap.empty[Symbol, (Double, Double, Double) => LoTurtle]
+  lazy val cmdsd = collection.mutable.HashMap.empty[Symbol, Any => LoTurtle]
+  lazy val cmdsdd = collection.mutable.HashMap.empty[Symbol, (Any, Any) => LoTurtle]
+  lazy val cmdsddd = collection.mutable.HashMap.empty[Symbol, (Any, Any, Any) => LoTurtle]
   var lastCmd = Symbol("")
 
-  def fd(n: Double) = { t.forward(n); this }
-  def bk(n: Double) = { t.back(n); this }
-  def rt(a: Double) = { t.right(a); this }
-  def rt(a: Double, r: Double) = { t.right(a, r); this }
-  def lt(a: Double) = { t.left(a); this }
-  def lt(a: Double, r: Double) = { t.left(a, r); this }
-  def hp(n: Double) = { t.hop(n); this }
-  def pc(c: Color) = { t.setPenColor(c); this }
-  def fc(c: Color) = { t.setFillColor(c); this }
-  def pt(n: Int) = { t.setPenThickness(n); this }
-  def jb(x: Double, y: Double) = { this hp x rt 90 hp y lt 90; this }
-  def jt(x: Double, y: Double) = { t.setPosition(x, y); this }
+  def toD(n: Any): Double = n match {
+    case i: Int    => i.toDouble
+    case d: Double => d
+    case _         => n.asInstanceOf[Double]
+  }
   def delay(n: Int) = { t.setAnimationDelay(n); this }
-  def speed(n: Double) = {
-    delay(math.max(math.min(Kmath.map(n, 1, 10, 1000, 0), 1000), 0).toInt)
+
+  def fd(n: Any) = { t.forward(toD(n)); this }
+  def bk(n: Any) = { t.back(toD(n)); this }
+  def rt(a: Any) = { t.right(toD(a)); this }
+  def rt(a: Any, r: Any) = { t.right(toD(a), toD(r)); this }
+  def lt(a: Any) = { t.left(toD(a)); this }
+  def lt(a: Any, r: Any) = { t.left(toD(a), toD(r)); this }
+  def hp(n: Any) = { t.hop(toD(n)); this }
+  def pc(c: Any) = { t.setPenColor(c.asInstanceOf[Color]); this }
+  def fc(c: Any) = { t.setFillColor(c.asInstanceOf[Color]); this }
+  def pt(n: Any) = { t.setPenThickness(n.asInstanceOf[Int]); this }
+  def jb(x: Any, y: Any) = { this hp x rt 90 hp y lt 90; this }
+  def jt(x: Any, y: Any) = { t.setPosition(toD(x), toD(y)); this }
+  def speed(n: Any) = {
+    delay(math.max(math.min(Kmath.map(toD(n), 1, 10, 1000, 0), 1000), 0).toInt)
   }
 
   def cmds(code: => LoTurtle) {
     cmdsu.put(lastCmd, { x: Unit => code })
   }
-  def cmds(code: Double => LoTurtle) {
+  def cmds(code: Any => LoTurtle) {
     cmdsd.put(lastCmd, code)
   }
-  def cmds(code: (Double, Double) => LoTurtle) {
+  def cmds(code: (Any, Any) => LoTurtle) {
     cmdsdd.put(lastCmd, code)
   }
 
-  def cmds(code: (Double, Double, Double) => LoTurtle) {
+  def cmds(code: (Any, Any, Any) => LoTurtle) {
     cmdsddd.put(lastCmd, code)
   }
 
@@ -55,7 +61,7 @@ class LoTurtle(val t: core.Turtle) extends Dynamic {
   //    cmdsu(Symbol(name)).apply(())
   //  }
 
-  def applyDynamic(name: String)(args: Double*): LoTurtle = {
+  def applyDynamic(name: String)(args: Any*): LoTurtle = {
     try {
       args.size match {
         case 0 => cmdsu(Symbol(name)).apply(())
