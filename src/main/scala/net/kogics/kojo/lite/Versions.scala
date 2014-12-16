684c784704065db00a39d44f1ab2c000fe6ab63b
Lalit Pant <pant.lalit@gmail.com>
2014-12-16 13:57:40 +0530

    Logo-turtle: (1) introduce versions of left and right that take a turning radius. (2) constrain speed cmd to a delay range of 0 to 1000.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.04"
-  val KojoRevision = "r23"
-  val KojoBuildDate = "15 Dec 2014"
+  val KojoRevision = "r24"
+  val KojoBuildDate = "16 Dec 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
