bdf9c612678d88a11139021e622eff218b8429fd
Lalit Pant <pant.lalit@gmail.com>
2014-12-16 20:40:46 +0530

    Make types even more dymanic, to allow definition of new commands that can take Any params (at the cost of static, line-specific, error reporting).

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,7 +3,7 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.04"
-  val KojoRevision = "r24"
+  val KojoRevision = "r25"
   val KojoBuildDate = "16 Dec 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
