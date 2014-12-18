58321aa03c1bb62d31541a6713874794b638ff91
Lalit Pant <pant.lalit@gmail.com>
2014-12-18 12:20:35 +0530

    LoTurtle tweaks.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.04"
-  val KojoRevision = "r28"
-  val KojoBuildDate = "17 Dec 2014"
+  val KojoRevision = "r29"
+  val KojoBuildDate = "18 Dec 2014"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
