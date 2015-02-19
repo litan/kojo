213b63e4db6f43155db2d89ee85d6bd44d87ceda
Lalit Pant <pant.lalit@gmail.com>
2015-02-20 00:15:05 +0530

    i18n tweaks.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.05"
-  val KojoRevision = "r10"
-  val KojoBuildDate = "18 Feb 2015"
+  val KojoRevision = "r11"
+  val KojoBuildDate = "19 Feb 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
