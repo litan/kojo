728ae56aefc7ea342edde44ee14f780945a68e5b
Lalit Pant <lalit_pant@yahoo.com>
2015-02-21 09:26:57 +0530

    Fix - if there is an animation in a script the animation start callback should be called once and only once.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.05"
-  val KojoRevision = "r12"
-  val KojoBuildDate = "20 Feb 2015"
+  val KojoRevision = "r13"
+  val KojoBuildDate = "21 Feb 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
