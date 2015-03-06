83794b2542c7959e24b444217dcce5a6fc729bbb
Lalit Pant <lalit_pant@yahoo.com>
2015-03-06 14:10:34 +0530

    Upgrade to Scala 2.11.6

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,9 +2,9 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
-  val KojoVersion = "2.4.05"
-  val KojoRevision = "r15"
-  val KojoBuildDate = "01 Mar 2015"
+  val KojoVersion = "2.4.06"
+  val KojoRevision = "r1"
+  val KojoBuildDate = "06 Mar 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
