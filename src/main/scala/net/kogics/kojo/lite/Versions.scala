76a606bb9b307e6beaa35ef55971a3806d01bb78
Lalit Pant <lalit_pant@yahoo.com>
2015-03-28 15:27:07 +0530

    Tweak picture bouncing off picture stuff.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.06"
-  val KojoRevision = "r14"
-  val KojoBuildDate = "27 Mar 2015"
+  val KojoRevision = "r15"
+  val KojoBuildDate = "28 Mar 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
