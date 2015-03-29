9236071f258d3980de962bbb37f5ca24a698748e
Lalit Pant <pant.lalit@gmail.com>
2015-03-29 19:42:12 +0530

    Refine picture bouncing off picture stuff.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,7 +3,7 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.06"
-  val KojoRevision = "r16"
+  val KojoRevision = "r17"
   val KojoBuildDate = "29 Mar 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
