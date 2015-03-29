bd40ce21bf46b173f23eb3e85b7c94d6405199f2
Lalit Pant <pant.lalit@gmail.com>
2015-03-29 15:10:28 +0530

    Refine picture bouncing off picture stuff.

--- src/test/scala/net/kogics/kojo/picture/PictureBounceTest.scala
+++ src/test/scala/net/kogics/kojo/picture/PictureBounceTest.scala
@@ -20,11 +20,36 @@ class PictureBounceTest extends FunSuite with Matchers {
     moveTo(x2, y2)
   }
 
+  def stageCorner = Pic { t =>
+    import t._
+    right(90)
+    forward(200)
+    left(90)
+    forward(200)
+  }
+
   test("bounce 1") {
     val obj = picline(0, 0, 100, 0)
     val pic = trans(50, -20) -> circle(20)
     obj.draw(); pic.draw();
     val v = Vector2D(0, 100)
     bouncePicVectorOffPic(pic, v, obj, rg) shouldBe Vector2D(0, -100)
+  }
+
+  test("bounce ball of stage corner") {
+    val obj = stageCorner
+    val ball = trans(182, 18) -> circle(20)
+    obj.draw(); ball.draw();
+    val v = Vector2D(100, -100)
+    bouncePicVectorOffPic(ball, v, obj, rg) shouldBe Vector2D(-100, 100)
+  }
+
+  test("bounce rectangle of circle") {
+    val d = math.sin(45.toRadians) * (40 + math.sqrt(400 + 400))
+    val obj = trans(d, d) -> circle(40)
+    val rec = trans(2, 2) -> rect(20, 20)
+    obj.draw(); rec.draw();
+    val v = Vector2D(50, 50)
+    bouncePicVectorOffPic(rec, v, obj, rg) shouldBe Vector2D(-50, -50)
   }
 }
