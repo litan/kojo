e465caba5ac80bdb0278106059eb13e65cfa2f34
Lalit Pant <pant.lalit@gmail.com>
2015-04-06 14:35:17 +0530

    Collidium tweak.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,7 +3,7 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.06"
-  val KojoRevision = "r28"
+  val KojoRevision = "r29"
   val KojoBuildDate = "6 Apr 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
