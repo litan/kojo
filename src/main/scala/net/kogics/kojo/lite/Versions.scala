17acd648d5878d6e40dca40e38fb998909f69f32
Lalit Pant <pant.lalit@gmail.com>
2015-08-21 21:34:15 +0530

    Russian Level 1 translation by Aleksei Loginov.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,9 +2,9 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
-  val KojoVersion = "2.4.06"
-  val KojoRevision = "r41"
-  val KojoBuildDate = "20 Apr 2015"
+  val KojoVersion = "2.4.07"
+  val KojoRevision = "r1"
+  val KojoBuildDate = "21 Aug 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
