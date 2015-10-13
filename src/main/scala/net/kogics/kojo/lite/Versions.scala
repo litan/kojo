fdad3f8ef25c4a734ebc8540143a072723d0da55
Lalit Pant <lalit_pant@yahoo.com>
2015-10-13 22:27:30 +0530

    Italian Inputs from Massimo.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,7 +3,7 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.08"
-  val KojoRevision = "r3"
+  val KojoRevision = "r4"
   val KojoBuildDate = "13 October 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
