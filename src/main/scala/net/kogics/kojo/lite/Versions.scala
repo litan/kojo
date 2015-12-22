a52f0ca3e624c32aeaf217331257837141d478ad
Lalit Pant <pant.lalit@gmail.com>
2015-12-22 21:38:59 +0530

    Italian: New menu items + setSpeed translations (contributed by Massimo).

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.09"
-  val KojoRevision = "r1"
-  val KojoBuildDate = "8 December 2015"
+  val KojoRevision = "r2"
+  val KojoBuildDate = "22 December 2015"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
