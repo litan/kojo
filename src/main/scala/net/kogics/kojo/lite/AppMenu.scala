98fea0f622ed9a45bcc4d572ba70f98749cdbf30
Lalit Pant <pant.lalit@gmail.com>
2016-02-05 19:10:27 +0530

    Update copyright year.

--- src/main/scala/net/kogics/kojo/lite/AppMenu.scala
+++ src/main/scala/net/kogics/kojo/lite/AppMenu.scala
@@ -389,7 +389,7 @@ trait AppMenu {
               Version: ${Versions.KojoVersion}  <em>${Versions.KojoRevision}</em><br/>
               Build date: ${Versions.KojoBuildDate}<br/>
               <em>Java version: ${Versions.JavaVersion}. Scala version: ${Versions.ScalaVersion}</em> <br/><br/>
-              Copyright &copy; 2009-2014 Lalit Pant (pant.lalit@gmail.com) and the Kojo Dev Team.<br/><br/>
+              Copyright &copy; 2009-2016 Lalit Pant (pant.lalit@gmail.com) and the Kojo Dev Team.<br/><br/>
               Please visit <em>http://www.kogics.net/kojo</em> for more information about Kojo.<br/><br/>
               <strong>Kojo</strong> Contributors:<ul>
                <li>Lalit Pant</li>
