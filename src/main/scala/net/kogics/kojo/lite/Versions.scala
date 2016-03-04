17bc473da7fc003345c0c2bf410eeb2b4445c384
Lalit Pant <pant.lalit@gmail.com>
2016-03-04 15:52:01 +0530

    Tweak fractions game + incorporate menu translations.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.09"
-  val KojoRevision = "r4"
-  val KojoBuildDate = "5 February 2016"
+  val KojoRevision = "r5"
+  val KojoBuildDate = "3 March 2016"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
