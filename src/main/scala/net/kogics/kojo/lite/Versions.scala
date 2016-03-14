66cd124764134f87cf25dfc4a91d41505673cbf7
Lalit Pant <pant.lalit@gmail.com>
2016-03-14 15:58:14 +0530

    Incorporate Italian translations.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.4 Beta"
   val KojoVersion = "2.4.09"
-  val KojoRevision = "r7"
-  val KojoBuildDate = "11 March 2016"
+  val KojoRevision = "r8"
+  val KojoBuildDate = "14 March 2016"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
