6e037d4364ef01d32f62c05fbfba2aba5d7e5a16
Lalit Pant <pant.lalit@gmail.com>
2016-09-04 21:20:24 +0530

    Update contributor list and tweak gitignore

--- src/main/scala/net/kogics/kojo/lite/AppMenu.scala
+++ src/main/scala/net/kogics/kojo/lite/AppMenu.scala
@@ -418,6 +418,7 @@ trait AppMenu {
                <li>Vipul Pandey</li>
                <li>Aleksei Loginov</li>
                <li>Massimo Maria Ghisalberti</li>
+               <li>Aditya Pant</li>
               </ul>
               <strong>Kojo</strong> is licensed under The GNU General Public License (GPL). The full text of the GPL is available at: http://www.gnu.org/licenses/gpl.html<br/><br/>
               Kojo runs on the Java Platform.<br/><br/>
