75a482e34ba5586cb14353c0f5425de41c43168c
Lalit Pant <pant.lalit@gmail.com>
2016-09-04 21:18:33 +0530

    Bump up version number.

--- makeinstaller.sh
+++ makeinstaller.sh
@@ -2,7 +2,7 @@ source ~/bin/activate-java-16.sh
 java -version
 set -x 
 # Build Kojo
-# ./sbt.sh clean package
+./sbt.sh clean package
 
 # Create installer
 rm -rf installerbuild
--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -1,10 +1,10 @@
 package net.kogics.kojo.lite
 
 object Versions {
-  val KojoMajorVersion = "2.4 Beta"
-  val KojoVersion = "2.4.09"
-  val KojoRevision = "r8"
-  val KojoBuildDate = "14 March 2016"
+  val KojoMajorVersion = "2.4"
+  val KojoVersion = "2.4.10"
+  val KojoRevision = "r1"
+  val KojoBuildDate = "4 Sept 2016"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
