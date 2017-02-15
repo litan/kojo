9853f81b14c36c9f4a16f7c14299e26e626b3f99
Lalit Pant <pant.lalit@gmail.com>
2017-02-15 18:22:46 +0530

    Allow user control of font-size via kojo.properties.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,9 +2,9 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.4"
-  val KojoVersion = "2.4.10"
-  val KojoRevision = "r2"
-  val KojoBuildDate = "16 Oct 2016"
+  val KojoVersion = "2.4.11"
+  val KojoRevision = "r1"
+  val KojoBuildDate = "15 Feb 2017"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
