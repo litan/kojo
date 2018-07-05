5373d9d829b6b387528eabaaa3fe5b305442cc51
Lalit Pant <pant.lalit@gmail.com>
2018-07-05 18:25:03 +0530

    Still use Java 6 while building old IzPack installer (to suppress a flood of 'Passing class file uncompressed due to unrecognized attribute' warnings).

--- makeinstaller.sh
+++ makeinstaller.sh
@@ -14,9 +14,13 @@ cd ..
 # run IzPack to create installer
 cp -var installer/* installerbuild/
 cd installerbuild
+
+# Still need Java 6 for IzPack to get around "unable to compress" warning
+source ~/bin/activate-java-16.sh
 ~/tools/IzPack/bin/compile install.xml
+
 mv install.jar kojoInstall.jar
 echo jar installer generated -- installerbuild/kojoInstall.jar
-~/tools/launch4j/launch4j winInstaller.xml
+
 
 
