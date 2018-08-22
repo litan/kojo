6af955375b803da3e0c159bcd92c1d6519c44614
Lalit Pant <pant.lalit@gmail.com>
2018-08-22 09:29:41 +0530

    Bump up version number (2.7.02 r20).

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.7"
   val KojoVersion = "2.7.02"
-  val KojoRevision = "r19"
-  val KojoBuildDate = "19 August 2018"
+  val KojoRevision = "r20"
+  val KojoBuildDate = "22 August 2018"
   val JavaVersion = geogebra.main.AppD.getJavaVersion
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
