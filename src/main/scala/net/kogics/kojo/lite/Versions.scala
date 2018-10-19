dc845b32e1dc455e8ca5f0ad119f9b6171cbe5f9
Lalit Pant <pant.lalit@gmail.com>
2018-10-19 10:51:35 +0530

    Tweak Java version string.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -5,6 +5,16 @@ object Versions {
   val KojoVersion = "2.7.04"
   val KojoRevision = "r7"
   val KojoBuildDate = "10 October 2018"
-  val JavaVersion = geogebra.main.AppD.getJavaVersion
+  val JavaVersion = {
+    val jrv = System.getProperty("java.runtime.version")
+    val arch = System.getProperty("os.arch")
+    if (jrv == null) {
+      val jv = System.getProperty("java.version")
+      s"$jv; $arch"
+    }
+    else {
+      s"$jrv; $arch"
+    }
+  }
   val ScalaVersion = scala.tools.nsc.Properties.versionString.substring("version ".length)
 }
