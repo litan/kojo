4ddbb23f6e92943bbe645bd0bd8c53c061c6409e
Lalit Pant <pant.lalit@gmail.com>
2019-09-08 20:08:17 +0530

    Upgrade Scala to 2.12.9

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,9 +2,9 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.7"
-  val KojoVersion = "2.7.06"
-  val KojoRevision = "r8"
-  val KojoBuildDate = "24 August 2019"
+  val KojoVersion = "2.7.07"
+  val KojoRevision = "r1"
+  val KojoBuildDate = "08 September 2019"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
