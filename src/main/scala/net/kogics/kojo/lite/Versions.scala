8f71ecde56f153925472b543aeb837085b75c464
Lalit Pant <pant.lalit@gmail.com>
2019-10-23 08:07:16 +0530

    Integrate language translations.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.7"
   val KojoVersion = "2.7.08"
-  val KojoRevision = "r12"
-  val KojoBuildDate = "21 October 2019"
+  val KojoRevision = "r13"
+  val KojoBuildDate = "23 October 2019"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
