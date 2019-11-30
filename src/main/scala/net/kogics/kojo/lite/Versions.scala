04014b405e33edbc5ddae0d563d33b4e3d2e2318
Lalit Pant <pant.lalit@gmail.com>
2019-11-30 11:28:38 +0530

    Clear (potentially backlogged under load) gui queue on clear().

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.7"
   val KojoVersion = "2.7.09"
-  val KojoRevision = "r17"
-  val KojoBuildDate = "27 November 2019"
+  val KojoRevision = "r18"
+  val KojoBuildDate = "30 November 2019"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
