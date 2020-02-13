7060631a44269c8f94396635eace8b10d18a0f2f
Lalit Pant <pant.lalit@gmail.com>
2020-02-13 09:56:39 +0530

    Tweak Breakpoint pane.

--- src/main/scala/net/kogics/kojo/lite/BreakpointPane.scala
+++ src/main/scala/net/kogics/kojo/lite/BreakpointPane.scala
@@ -7,6 +7,7 @@ import javax.swing.JDialog
 import javax.swing.JFrame
 import javax.swing.JLabel
 import javax.swing.JScrollPane
+import javax.swing.ScrollPaneConstants
 import javax.swing.border.EmptyBorder
 
 import net.kogics.kojo.core
@@ -53,28 +54,38 @@ object BreakpointPane {
 class BreakpointPane(owner: JFrame) extends JDialog(owner) {
   var btnPressed = false
 
-  setTitle("Breakpoint")
+  setTitle("Breakpoint Message")
   setModal(true)
+
+  val bkptMsglabel = new JLabel()
+  val sp = new JScrollPane(bkptMsglabel)
+  sp.setBorder(new EmptyBorder(15, 10, 5, 10))
+  sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+  val resumeCancelLabel = new JLabel()
+  resumeCancelLabel.setAlignmentX(Component.CENTER_ALIGNMENT)
+  resumeCancelLabel.setBorder(new EmptyBorder(10, 10, 10, 10))
+
   val d = new JButton("Resume")
   d.setAlignmentX(Component.CENTER_ALIGNMENT)
   d.addActionListener { _ =>
     btnPressed = true
     setVisible(false)
   }
-  val label = new JLabel()
-  label.setBorder(new EmptyBorder(0, 10, 0, 10))
-  val sp = new JScrollPane(label)
   getRootPane.setDefaultButton(d)
-  val panel = ColPanel(sp, d)
+
+  val panel = ColPanel(sp, resumeCancelLabel, d)
   getContentPane.add(panel)
-  setSize(400, 120)
-  setLocationRelativeTo(owner)
 
   def show(msg: Any, resumeMsg: String): Int = {
     setVisible(false)
     btnPressed = false
-    label.setText(msg.toString)
-    setTitle(resumeMsg)
+    bkptMsglabel.setText(msg.toString)
+    resumeCancelLabel.setText(resumeMsg + ", Escape to cancel")
+    pack()
+    val sz = getSize
+    setSize(500, sz.height)
+    setLocationRelativeTo(owner)
     setVisible(true)
     if (btnPressed) 1 else 0
   }
--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.7"
   val KojoVersion = "2.7.11"
-  val KojoRevision = "r6"
-  val KojoBuildDate = "12 February 2020"
+  val KojoRevision = "r7"
+  val KojoBuildDate = "13 February 2020"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
