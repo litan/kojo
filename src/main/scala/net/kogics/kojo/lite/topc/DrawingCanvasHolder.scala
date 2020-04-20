450838a7df78a5606c197a40ab3fc231723f8655
Lalit Pant <pant.lalit@gmail.com>
2020-04-20 18:39:13 +0530

    Take care of the flicker of white on the Canvas during startup.

--- src/main/scala/net/kogics/kojo/lite/topc/DrawingCanvasHolder.scala
+++ src/main/scala/net/kogics/kojo/lite/topc/DrawingCanvasHolder.scala
@@ -1,20 +1,18 @@
 package net.kogics.kojo.lite.topc
 
 import net.kogics.kojo.core.KojoCtx
+import net.kogics.kojo.lite.Theme
 import net.kogics.kojo.lite.canvas.SpriteCanvas
 import net.kogics.kojo.util.Utils
 
-import bibliothek.gui.dock.common.event.CFocusListener
-import bibliothek.gui.dock.common.intern.CDockable
-
 class DrawingCanvasHolder(val dc: SpriteCanvas, ctx: KojoCtx)
   extends BaseHolder("DC", Utils.loadString("CTL_SCanvasTopComponent"), dc) {
-
+  dc.setBackground(Theme.currentTheme.canvasBg)
   def activate(): Unit = {
     toFront()
     dc.activate()
   }
-  
+
   // called when only the canvas and not the holder needs to be activate (e.g when Fullscreen-ing)
   def activateCanvas(): Unit = {
     dc.activate()
