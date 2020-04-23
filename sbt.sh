fe6f24a2ab13e57fad948ece8abf4de677b9ea8e
Lalit Pant <pant.lalit@gmail.com>
2020-04-23 20:05:46 +0530

    Bump up sbt to 0.13.18

--- sbt.sh
+++ sbt.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -jar sbt-launch.0.13.16.jar "$@"
+java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -jar sbt-launch.0.13.18.jar "$@"
