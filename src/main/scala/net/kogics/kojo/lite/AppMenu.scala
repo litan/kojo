9d538f23b6a1de849f79e3a39ee8966ef02d61b8
Lalit Pant <pant.lalit@gmail.com>
2021-01-11 19:35:39 +0530

    Add Guillermo's name to contributers (for the Spanish translation).

--- src/main/scala/net/kogics/kojo/lite/AppMenu.scala
+++ src/main/scala/net/kogics/kojo/lite/AppMenu.scala
@@ -473,6 +473,7 @@ trait AppMenu {
                <li>Luka Volaric</li>
                <li>Marcus Klang</li>
                <li>Bulent Basaran</li>
+               <li>Guillermo Ovejero</li>
               </ul>
               <strong>Kojo</strong> is licensed under The GNU General Public License (GPL). The full text of the GPL is available at: http://www.gnu.org/licenses/gpl.html<br/><br/>
               Kojo runs on the Java Platform.<br/><br/>
