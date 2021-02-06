4df3ea54b49feae3b05fc4951e4e8073e7119368
Lalit Pant <pant.lalit@gmail.com>
2021-02-06 10:28:08 +0530

    Tweak batch pics.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.9"
   val KojoVersion = "2.9.09"
-  val KojoRevision = "r2"
-  val KojoBuildDate = "15 January 2020"
+  val KojoRevision = "r3"
+  val KojoBuildDate = "6 February 2021"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
