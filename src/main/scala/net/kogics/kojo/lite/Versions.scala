362256ffca306e5a77babf342575ba0b57e16e6f
Lalit Pant <pant.lalit@gmail.com>
2021-10-03 13:31:06 +0530

    Speculatively try to fix a warning that was seen in the wild.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -2,9 +2,9 @@ package net.kogics.kojo.lite
 
 object Versions {
   val KojoMajorVersion = "2.9"
-  val KojoVersion = "2.9.12"
-  val KojoRevision = "r9"
-  val KojoBuildDate = "23 September 2021"
+  val KojoVersion = "2.9.13"
+  val KojoRevision = "r1"
+  val KojoBuildDate = "3 October 2021"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
