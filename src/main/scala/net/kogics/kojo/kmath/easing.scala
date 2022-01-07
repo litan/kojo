62d460559859f5c9e657bf28edb81d6ac705df5f
Lalit Pant <pant.lalit@gmail.com>
2022-01-07 18:32:00 +0530

    Add a couple of easing types (elasticX and bounceX).

--- src/main/scala/net/kogics/kojo/kmath/easing.scala
+++ src/main/scala/net/kogics/kojo/kmath/easing.scala
@@ -26,4 +26,34 @@ object KEasing {
     override def ease(start: Double, end: Double, step: Double, duration: Double): Double =
       Easing.QUAD_IN_OUT.ease(step.toFloat, start.toFloat, (end - start).toFloat, duration.toFloat)
   }
+
+  val ElasticIn = new KEasing {
+    override def ease(start: Double, end: Double, step: Double, duration: Double): Double =
+      Easing.ELASTIC_IN.ease(step.toFloat, start.toFloat, (end - start).toFloat, duration.toFloat)
+  }
+
+  val ElasticOut = new KEasing {
+    override def ease(start: Double, end: Double, step: Double, duration: Double): Double =
+      Easing.ELASTIC_OUT.ease(step.toFloat, start.toFloat, (end - start).toFloat, duration.toFloat)
+  }
+
+  val ElasticInOut = new KEasing {
+    override def ease(start: Double, end: Double, step: Double, duration: Double): Double =
+      Easing.ELASTIC_IN_OUT.ease(step.toFloat, start.toFloat, (end - start).toFloat, duration.toFloat)
+  }
+
+  val BounceIn = new KEasing {
+    override def ease(start: Double, end: Double, step: Double, duration: Double): Double =
+      Easing.BOUNCE_IN.ease(step.toFloat, start.toFloat, (end - start).toFloat, duration.toFloat)
+  }
+
+  val BounceOut = new KEasing {
+    override def ease(start: Double, end: Double, step: Double, duration: Double): Double =
+      Easing.BOUNCE_OUT.ease(step.toFloat, start.toFloat, (end - start).toFloat, duration.toFloat)
+  }
+
+  val BounceInOut = new KEasing {
+    override def ease(start: Double, end: Double, step: Double, duration: Double): Double =
+      Easing.BOUNCE_IN_OUT.ease(step.toFloat, start.toFloat, (end - start).toFloat, duration.toFloat)
+  }
 }
--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.9"
   val KojoVersion = "2.9.16"
-  val KojoRevision = "r11"
-  val KojoBuildDate = "6 January 2022"
+  val KojoRevision = "r12"
+  val KojoBuildDate = "7 January 2022"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
