ba76ace187bf74b8d458bdba8e0121b6dd52b907
Lalit Pant <pant.lalit@gmail.com>
2022-03-12 10:05:13 +0530

    Add chess include script contributed by Ida Rasmark.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.9"
   val KojoVersion = "2.9.19"
-  val KojoRevision = "r7"
-  val KojoBuildDate = "9 March 2022"
+  val KojoRevision = "r8"
+  val KojoBuildDate = "12 March 2022"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
