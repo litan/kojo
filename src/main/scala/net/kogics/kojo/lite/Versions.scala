f273bda23c7b152999014efb19ee5b70a7eda4ee
Lalit Pant <pant.lalit@gmail.com>
2022-05-03 12:20:49 +0530

    Bump up FlatLAF to 2.2

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.9"
   val KojoVersion = "2.9.21"
-  val KojoRevision = "r1"
-  val KojoBuildDate = "1 May 2022"
+  val KojoRevision = "r2"
+  val KojoBuildDate = "3 May 2022"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
