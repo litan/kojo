4e674cf9eef18ea9398501fcadf3a7fe6704ddce
Lalit Pant <pant.lalit@gmail.com>
2022-07-01 20:19:19 +0530

    Tweak code completion (enable completions for first pic in a picStack etc).

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.9"
   val KojoVersion = "2.9.22"
-  val KojoRevision = "r3"
-  val KojoBuildDate = "26 June 2022"
+  val KojoRevision = "r4"
+  val KojoBuildDate = "1 July 2022"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
