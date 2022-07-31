ad67525891b2818f2babbfc0a220329d6c0d6a6e
Lalit Pant <pant.lalit@gmail.com>
2022-07-31 09:21:36 +0530

    Bump up revision number.

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.9"
   val KojoVersion = "2.9.23"
-  val KojoRevision = "r1"
-  val KojoBuildDate = "27 July 2022"
+  val KojoRevision = "r2"
+  val KojoBuildDate = "31 July 2022"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
