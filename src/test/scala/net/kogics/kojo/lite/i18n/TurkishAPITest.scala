390d6dcc703874a0025dfc1b59a3e8b1d81f7df7
Ben Bulent Basaran <ben@scala.org>
2022-08-15 16:50:54 -0400

    Fix a test issue. Failed on my sbt testing between two runs getting:   buSaniye2=44410.149872524 or,   buSaniye2=78071.307503878 [info] - Translation of java.util.Calendar and System.nanoTime etc to work *** FAILED *** [info]   false was not true (TurkishAPITest.scala:688) Fixed using a much more conservative value.

--- src/test/scala/net/kogics/kojo/lite/i18n/TurkishAPITest.scala
+++ src/test/scala/net/kogics/kojo/lite/i18n/TurkishAPITest.scala
@@ -684,8 +684,10 @@ import net.kogics.kojo.staging
       saat >= 0 && saat <= 24 should be(true)
     }
     buSaniye > 1.0E9 should be(true)
-    buSaniye2 > 83646.86 should be(true)
-    buAn > 1659458389799L should be(true)
+    println(s"buSaniye2=$buSaniye2")
+    // used to get > 80k, but now I get only > 44k. Let's be safe:
+    buSaniye2 > 4_000.0 should be(true) // Returns the current value of the running Java Virtual Machine's high-resolution time source, in nanoseconds.
+    buAn > 1_659_458_389_799L should be(true)
     buAn2 > İriSayı("1659457918531") should be(true)
     sayıyaKadarSay(5000) < 0.01 should be(true)
     sayıyaKadarSay(1000000) < 0.1 should be(true)
