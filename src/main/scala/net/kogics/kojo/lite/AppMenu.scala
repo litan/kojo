d81e74ba6ab99d084cbe43eff51d70e1f6a740e1
Lalit Pant <pant.lalit@gmail.com>
2023-11-20 09:51:21 +0530

    Add Vasu and Mudit as contributors.

--- src/main/scala/net/kogics/kojo/lite/AppMenu.scala
+++ src/main/scala/net/kogics/kojo/lite/AppMenu.scala
@@ -493,6 +493,8 @@ trait AppMenu {
                <li>Guillermo Ovejero</li>
                <li>Alberto R.R. Manzanares</li>
                <li>Anay Kamat</li>
+               <li>Vasu Sethia</li>
+               <li>Mudit Pant</li>
               </ul>
               <strong>Kojo</strong> is licensed under The GNU General Public License (GPL). The full text of the GPL is available at: http://www.gnu.org/licenses/gpl.html<br/><br/>
               Kojo runs on the Java Platform.<br/><br/>
