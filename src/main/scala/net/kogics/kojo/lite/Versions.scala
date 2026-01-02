cd29d3ec103c82e670a550955fcd0e3d6a929da7
Lalit Pant <pant.lalit@gmail.com>
2026-01-02 17:24:42 +0530

    Bump up Kojo version to 2.9.34

--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoMajorVersion = "2.9"
   val KojoVersion = "2.9.34"
-  val KojoRevision = "r2"
-  val KojoBuildDate = "2 Jan 2025"
+  val KojoRevision = "r3"
+  val KojoBuildDate = "2 Jan 2026"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
